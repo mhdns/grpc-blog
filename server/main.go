1e024d931307c407483c9fbe89ca193782504edf
Muhammad Anas <muhd.anas@outlook.com>
2020-07-09 10:33:56 +0800

    added update route

--- server/main.go
+++ server/main.go
@@ -44,18 +44,18 @@ func (s *server) CreateBlog(ctx context.Context, req *blogpb.CreateBlogRequest) 
 	}
 
 	// Remove later
-	rows, err := s.db.Query("Select * from blog;")
-	if err != nil {
-		fmt.Println("Error ", err)
-	}
+	// rows, err := s.db.Query("Select * from blog;")
+	// if err != nil {
+	// 	fmt.Println("Error ", err)
+	// }
 
-	for rows.Next() {
-		var id, title, createdAt, post string
-		if err := rows.Scan(&id, &title, &createdAt, &post); err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println(id, title, createdAt, post)
-	}
+	// for rows.Next() {
+	// 	var id, title, createdAt, post string
+	// 	if err := rows.Scan(&id, &title, &createdAt, &post); err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// 	fmt.Println(id, title, createdAt, post)
+	// }
 
 	return &blogpb.CreateBlogResponse{
 		Blog: &blogpb.Blog{
@@ -101,8 +101,37 @@ func (s *server) GetBlog(ctx context.Context, req *blogpb.GetBlogRequest) (*blog
 }
 
 func (s *server) UpdateBlog(ctx context.Context, req *blogpb.UpdateBlogRequest) (*blogpb.UpdateBlogResponse, error) {
+	updateID := req.GetBlogId()
+	updateTitle := req.GetBlog().GetTitle()
+	updatePost := req.GetBlog().GetPost()
 
-	return &blogpb.UpdateBlogResponse{}, nil
+	query := readSQL("queries/update_blog.sql")
+	stmt, err := s.db.Prepare(query)
+	if err != nil {
+		return nil, status.Errorf(codes.Internal, "database error: %v", err.Error())
+	}
+	defer stmt.Close()
+
+	var id, title, createdAt, post string
+	err = stmt.QueryRowContext(context.Background(), updateID, updateTitle,
+		updatePost).Scan(&id, &title, &createdAt, &post)
+	if err == sql.ErrNoRows {
+		fmt.Println(err)
+		return nil, status.Errorf(codes.NotFound, "blog with id %v not found: ", updateID, err.Error())
+	} else if err != nil {
+		return nil, status.Errorf(codes.Internal, "database err: %v", err.Error())
+	}
+
+	return &blogpb.UpdateBlogResponse{
+		Blog: &blogpb.Blog{
+			Id:    id,
+			Title: title,
+			Date:  createdAt,
+			Post:  post,
+		},
+		Msg:     "updated one blog successfully",
+		Success: true,
+	}, nil
 }
 
 func (s *server) DeleteBlog(ctx context.Context, req *blogpb.DeleteBlogRequest) (*blogpb.DeleteBlogResponse, error) {
