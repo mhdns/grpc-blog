5dd19cb80efed61cf9dd4d554a2fa2afad2b584b
Muhammad Anas <muhd.anas@outlook.com>
2020-07-18 12:17:43 +0800

    crud methods implemented for user grpc

--- server/user.go
+++ server/user.go
@@ -3,6 +3,7 @@ package main
 import (
 	"blog/server/userpb"
 	"context"
+	"fmt"
 	"log"
 )
 
@@ -44,13 +45,77 @@ func (s *server) CreateUser(ctx context.Context, req *userpb.CreateUserRequest) 
 }
 
 func (s *server) GetUser(ctx context.Context, req *userpb.GetUserRequest) (*userpb.GetUserResponse, error) {
-	return &userpb.GetUserResponse{}, nil
+	id := req.GetUserId()
+
+	query := readSQL("queries/get_user.sql")
+	stmt, err := s.db.Prepare(query)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	row := stmt.QueryRowContext(ctx, id)
+
+	var returnedID, returnedName, returnedEmail, returnedPW, returnedSalt string
+
+	err = row.Scan(returnedID, returnedName, returnedEmail, returnedPW, returnedSalt)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return &userpb.GetUserResponse{
+		User: &userpb.User{
+			Id:   returnedID,
+			Name: returnedName,
+		},
+		Msg:     "user retrieved successfully",
+		Success: true,
+	}, nil
 }
 
 func (s *server) UpdateUser(ctx context.Context, req *userpb.UpdateUserRequest) (*userpb.UpdateUserResponse, error) {
-	return &userpb.UpdateUserResponse{}, nil
+	id := req.GetUserId()
+	newName := req.GetUser().GetName()
+
+	query := readSQL("queries/update_user.sql")
+	stmt, err := s.db.Prepare(query)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	row := stmt.QueryRowContext(ctx, id, newName, "random@something.com")
+
+	var returnedID, returnedName string
+
+	err = row.Scan(returnedID, returnedName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return &userpb.UpdateUserResponse{
+		User: &userpb.User{
+			Id:   returnedID,
+			Name: returnedName,
+		},
+		Msg:     "updated user successfully",
+		Success: true,
+	}, nil
 }
 
 func (s *server) DeleteUser(ctx context.Context, req *userpb.DeleteUserRequest) (*userpb.DeleteUserResponse, error) {
-	return &userpb.DeleteUserResponse{}, nil
+	id := req.GetUserId()
+	query := readSQL("query/delete_user.sql")
+
+	stmt, err := s.db.Prepare(query)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+
+	result, err := stmt.ExecContext(ctx, id)
+
+	rowsAffected, _ := result.RowsAffected()
+	return &userpb.DeleteUserResponse{
+		Msg:     fmt.Sprintf("deleted %v user", rowsAffected),
+		Success: true,
+	}, nil
 }
