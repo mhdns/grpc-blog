8d66aef64fb180cd7cc72425be58d0b63a5a7f1e
Muhammad Anas <muhd.anas@outlook.com>
2020-07-20 16:15:10 +0800

    test user crud functionalities

--- client/client.go
+++ client/client.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"blog/blogpb"
+	"blog/server/userpb"
 	"context"
 	"fmt"
 	"log"
@@ -18,29 +19,52 @@ func main() {
 	}
 	defer conn.Close()
 
-	req := &blogpb.CreateBlogRequest{
-		Blog: &blogpb.Blog{
-			Title: "First Blog - Anas",
-			Post:  "This is my first blog. I hope everyone likes it. If you get to read it, good for you!!!!!",
-		},
-	}
+	// User portion
+	createUser(conn, &userpb.CreateUserRequest{
+		Email:    "abc@gmail.com",
+		Name:     "Ang Beng Chong",
+		Password: "123456",
+	})
 
-	createBlog(conn, req)
-	createBlog(conn, req)
-	createBlog(conn, req)
+	getUser(conn, &userpb.GetUserRequest{
+		UserId: "1",
+	})
 
-	getBlog(conn, &blogpb.GetBlogRequest{BlogId: "1"})
-
-	updateBlog(conn, &blogpb.UpdateBlogRequest{
-		BlogId: "1",
-		Blog: &blogpb.Blog{
-			Title: "Some New Title",
-			Post:  "This is a new post",
+	updateUser(conn, &userpb.UpdateUserRequest{
+		UserId: "1",
+		User: &userpb.User{
+			Id:   "1",
+			Name: "Ang Beng Cheng",
 		},
 	})
 
-	deleteBlog(conn, &blogpb.DeleteBlogRequest{BlogId: "1"})
-	deleteBlog(conn, &blogpb.DeleteBlogRequest{BlogId: "1"})
+	deleteUser(conn, &userpb.DeleteUserRequest{
+		UserId: "1",
+	})
+	// Blog portion
+	// req := &blogpb.CreateBlogRequest{
+	// 	Blog: &blogpb.Blog{
+	// 		Title: "First Blog - Anas",
+	// 		Post:  "This is my first blog. I hope everyone likes it. If you get to read it, good for you!!!!!",
+	// 	},
+	// }
+
+	// createBlog(conn, req)
+	// createBlog(conn, req)
+	// createBlog(conn, req)
+
+	// getBlog(conn, &blogpb.GetBlogRequest{BlogId: "1"})
+
+	// updateBlog(conn, &blogpb.UpdateBlogRequest{
+	// 	BlogId: "1",
+	// 	Blog: &blogpb.Blog{
+	// 		Title: "Some New Title",
+	// 		Post:  "This is a new post",
+	// 	},
+	// })
+
+	// deleteBlog(conn, &blogpb.DeleteBlogRequest{BlogId: "1"})
+	// deleteBlog(conn, &blogpb.DeleteBlogRequest{BlogId: "1"})
 
 }
 
@@ -82,6 +106,50 @@ func deleteBlog(conn *grpc.ClientConn, req *blogpb.DeleteBlogRequest) error {
 	c := blogpb.NewBlogServiceClient(conn)
 
 	res, err := c.DeleteBlog(context.Background(), req)
+	if err != nil {
+		return err
+	}
+	fmt.Println(res)
+	return nil
+}
+
+func createUser(conn *grpc.ClientConn, req *userpb.CreateUserRequest) error {
+	c := userpb.NewUserServiceClient(conn)
+
+	res, err := c.CreateUser(context.Background(), req)
+	if err != nil {
+		return err
+	}
+	fmt.Println(res)
+	return nil
+}
+
+func getUser(conn *grpc.ClientConn, req *userpb.GetUserRequest) error {
+	c := userpb.NewUserServiceClient(conn)
+
+	res, err := c.GetUser(context.Background(), req)
+	if err != nil {
+		return err
+	}
+	fmt.Println(res)
+	return nil
+}
+
+func updateUser(conn *grpc.ClientConn, req *userpb.UpdateUserRequest) error {
+	c := userpb.NewUserServiceClient(conn)
+
+	res, err := c.UpdateUser(context.Background(), req)
+	if err != nil {
+		return err
+	}
+	fmt.Println(res)
+	return nil
+}
+
+func deleteUser(conn *grpc.ClientConn, req *userpb.DeleteUserRequest) error {
+	c := userpb.NewUserServiceClient(conn)
+
+	res, err := c.DeleteUser(context.Background(), req)
 	if err != nil {
 		return err
 	}
